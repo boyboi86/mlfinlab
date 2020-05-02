a7b338075be4e02729674d157047ca99b5eebcf1
Wei Xiang <boyboi86@users.noreply.github.com>
2020-05-02 21:36:38 +0800

    added fd plot

--- mlfinlab/features/fracdiff.py
+++ mlfinlab/features/fracdiff.py
@@ -8,6 +8,9 @@ process.
 import numpy as np
 import pandas as pd
 
+import matplotlib.pyplot as plt
+from statsmodels.tsa.stattools import adfuller
+
 
 class FractionalDifferentiation:
     """
@@ -197,6 +200,36 @@ class FractionalDifferentiation:
         output_df = pd.concat(output_df, axis=1)
         return output_df
 
+    def plot_min_ffd(close_prices: pd.Series, max_d = 2, pval_threshold: float = 0.05):
+        '''    
+        params: close_price: pd.Series
+        max_d: maximum value to differentiate (optional)
+        pval_threshold: p-value to pass ADF test
+        '''
+        if pval_threshold > 0.05:
+            print('p-value for ADF should be less than 0.05 to confirm for stationarity')
+            return
+        close_prices = close_prices.to_frame()
+        out = pd.DataFrame(columns=['adfStat', 'pVal', 'lags', 'nObs', '95% conf', 'corr'])
+        for d in np.linspace(0, max_d, 200):
+            df1 = np.log(close_prices[['close']]).resample('1D').last()  # downcast to daily obs        
+            df1.dropna(inplace=True)
+            df2 = frac_diff_ffd(df1, 
+                                diff_amt = d, 
+                                thresh = 0.01).dropna()
+            
+            corr = np.corrcoef(df1.loc[df2.index, 'close'], df2['close'])[0, 1]
+            df2 = adfuller(df2['close'], maxlag=1, regression='c', autolag=None)
+            out.loc[d] = list(df2[:4]) + [df2[4]['5%']] + [corr]  # with critical value
+            try:
+                if df2[1] <= pval_threshold:
+                    return d
+            except:
+                p('Something is wrong! Most likely required d value more than 2!')
+        out[['adfStat', 'corr']].plot(secondary_y='adfStat', figsize=(10, 8))
+        plt.axhline(out['95% conf'].mean(), linewidth=1, color='r', linestyle='dotted')
+        return
+
 
 def get_weights(diff_amt, size):
     """ This is a pass-through function """
@@ -238,3 +271,16 @@ def frac_diff_ffd(series, diff_amt, thresh=1e-5):
     :return: (pd.DataFrame) A data frame of differenced series
     """
     return FractionalDifferentiation.frac_diff_ffd(series, diff_amt, thresh)
+
+def plot_min_ffd(close_prices: pd.Series, max_d = 2, pval_threshold: float = 0.05):
+    """
+    Source: Chapter 5, AFML (section 5.5, page 83);
+    
+    plotting out adf stats as well as correlation graph, as well as minimum d value to use
+    
+    
+    params: close_price: pd.Series
+    max_d: maximum value to differentiate (optional)
+    pval_threshold: p-value to pass ADF test
+    """
+    return FractionalDifferentiation.plot_min_ffd(close_prices, max_d, pval_threshold)
