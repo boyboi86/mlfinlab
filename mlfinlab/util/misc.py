46425dfe8406b6c9a16322565e5b36dec4a8762c
Wei Xiang <boyboi86@users.noreply.github.com>
2020-05-02 23:14:50 +0800

    improve half-life for mean-reversion estimate

--- mlfinlab/util/misc.py
+++ mlfinlab/util/misc.py
@@ -18,3 +18,18 @@ def crop_data_frame_in_batches(df: pd.DataFrame, chunksize: int):
     for _, chunk in df.groupby(np.arange(len(df)) // chunksize):
         generator_object.append(chunk)
     return generator_object
+
+def reversion_spd():
+    """
+    find average speed of reversion
+    ln(2)/x = y
+    
+    where 
+    x = benchmark used
+    y = years requried for reversion to take place
+    """
+    #https://www.hindawi.com/journals/jps/2016/5191583/
+    #https://www.quantstart.com/articles/Basics-of-Statistical-Mean-Reversion-Testing/
+    #average time required by vertical barriers to hit 70% profitable rate
+    #work backwards to get hald life required
+    #find correlation matrix for lenght of vertical barrier vs profitablility
